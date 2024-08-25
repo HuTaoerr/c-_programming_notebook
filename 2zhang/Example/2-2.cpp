1ad534b7145888a780f9098389ad71e09417c83a
HuTaoerr <605228402@qq.com>
2024-08-25 23:35:31 +0800

    例题2

---
+++ 2zhang/Example/2-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << static_cast<int>(7.9) << endl;
+    cout << static_cast<double>(23) << endl;
+    cout << static_cast<double>(15) / 2 << endl;
+    cout << static_cast<int>(7.9) + 8<< endl;
+    return 0;
+}
