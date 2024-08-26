06262654f8a73d875d59a705e87828a86ffeacc5
HuTaoerr <605228402@qq.com>
2024-08-27 02:04:36 +0800

    T3

---
+++ part-02/Testing/T2-3.cpp
@@ -0,0 +1,63 @@
+/*
+
+编写一个程序，提示用户输入一个小数，然后输出与该数最接近的整数
+
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        double num;
+        cout << "Please enter a decimals , we outputs the nearest integer to the number: ";
+        cin >> num;
+
+        // 得实现类似四舍五入，还得有负数和零
+        int a;
+        int b;
+        if (num > 0)
+        {
+            a = static_cast<int>(num);
+            b = a + 1;
+
+            if ((b - num) > 0.5)
+            {
+                cout << "is : " << a << endl;
+            }
+            else if ((b - num) < 0.5)
+            {
+                cout << "is : " << b << endl;
+            }
+            else
+            {
+                cout << "have two answer : " << a << " and " << b << endl;
+            }
+        }
+        else if (num < 0)
+        {
+            a = static_cast<int>(num);
+            b = a - 1;
+
+            if ((b - num) > -0.5)
+            {
+                cout << "is : " << b << endl;
+            }
+            else if ((b - num) < -0.5)
+            {
+                cout << "is : " << a << endl;
+            }
+            else
+            {
+                cout << "have two answer : " << a << " and " << b << endl;
+            }
+        }
+        else
+        {
+            cout << "0 is itself : " << num << endl;
+        }
+    }
+    return 0;
+}
---
+++ part-02/Testing/T2-4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    cout << static_cast<int>(-9.3) << endl;
+    cout << static_cast<int>(-9.8) << endl;
+    cout << static_cast<int>(9.3) << endl;
+    
+    return 0;
+}
