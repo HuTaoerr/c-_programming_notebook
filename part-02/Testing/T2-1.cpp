3c63a3418fcf7263fea360183107fa9bb9a5b781
HuTaoerr <605228402@qq.com>
2024-08-27 00:11:29 +0800

    Testing

--- part-02/Testing/T2-1.cpp
+++ part-02/Testing/T2-1.cpp
@@ -1,11 +1,26 @@
+/*
+
+Write a program that produces the following output:
+**********************************
+* Programming Assignment 1 *
+* Computer Programming I *
+* Author: ??? *
+* Due Date: Thursday, Jan. 24 *
+**********************************
+
+*/
+
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+    cout << "**********************************" << endl;
+    cout << "*     Programming Assignment 1   *" << endl;
+    cout << "*      Computer Programming I    *" << endl;
+    cout << "*         Author: ???            *" << endl;
+    cout << "*   Due Date: Thursday, Jan. 24  *" << endl;
+    cout << "**********************************" << endl;
 
-    cout << 15 / 2;
     return 0;
 }
-
-
-
