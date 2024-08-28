b4e41a984d1a3e7c544edfdeb5b556e9ab2eaa5e
HuTaoerr <605228402@qq.com>
2024-08-28 21:50:38 +0800

    r

--- part-03/Example/E3-6.cpp
+++ part-03/Example/E3-6.cpp
@@ -1,8 +1,17 @@
+/*
+输入输出string类型
+getline(输入流变量， string变量)
 
-    #include <iostream> 
-    using namespace std; 
+ */
+#include <iostream>
+using namespace std;
 
-    int main()
-    {
-        return 0;
-    }
+int main()
+{
+    string mystring;
+    cout << "Enter some string: " << endl;
+    getline(cin, mystring);
+
+    cout << mystring << endl;
+    return 0;
+}
