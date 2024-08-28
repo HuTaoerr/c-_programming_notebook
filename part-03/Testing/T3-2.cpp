1f127d8239b6d7f0d8b8c67f9839c6511eb32150
HuTaoerr <605228402@qq.com>
2024-08-29 00:29:14 +0800

    T2

--- part-03/Testing/T3-2.cpp
+++ part-03/Testing/T3-2.cpp
@@ -1,8 +1,19 @@
+/*
+提示用户输入一个小数，程序将该小数四舍五入后保留2位小数
+*/
 
-    #include <iostream> 
-    using namespace std; 
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
-    int main()
-    {
-        return 0;
-    }
+int main()
+{
+    double num1;
+    cout << "Enter a decimal and you will get a decimal with a precision of 2:  " << flush;
+    cout << fixed << showpoint << setprecision(2);
+    cin >> num1;
+
+    cout << num1 << endl;
+
+    return 0;
+}
