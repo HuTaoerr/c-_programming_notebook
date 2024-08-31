6bef7387dfdefe279ba10d97dd6312b7e9e13142
HuTaoerr <605228402@qq.com>
2024-09-01 00:05:16 +0800

    T6

--- part-04/Testing/T4-5.cpp
+++ part-04/Testing/T4-5.cpp
@@ -5,4 +5,10 @@ int main()
     double result = 0.1 / 0;
     std::cout << "Result: " << result << std::endl;
     return 0;
-} /*  */
+} 
+
+
+/* 
+Result: inf
+
+ */
--- part-04/Testing/T4-6.cpp
+++ part-04/Testing/T4-6.cpp
@@ -1,8 +1,56 @@
+/*
+编写程序，提示用户输入笛卡尔品管中的某一点（x，y），程序输出该点的位置
+*/
+#include <iostream>
+using namespace std;
 
-    #include <iostream> 
-    using namespace std; 
+int main()
+{
+    int x, y;
+    cout << "输入两个整数，作为x和y： ";
+    cin >> x >> y;
+    cout << endl;
 
-    int main()
+    // 在原点上
+    if (x == 0 && y == 0)
     {
-        return 0;
+        cout << "( " << x << " , " << y << " )" << "在原点";
     }
+    // 在x轴上
+    else if (y == 0)
+    {
+        cout << "( " << x << " , " << y << " )" << "在X轴上";
+    }
+    // 在y轴上
+    else if (x == 0)
+    {
+        cout << "( " << x << " , " << y << " )" << "在Y轴上";
+    }
+    // 第一象限
+    else if (x > 0 && y > 0)
+    {
+        cout << "( " << x << " , " << y << " )" << "在第一象限";
+    }
+    // 第二象限
+    else if (x < 0 && y > 0)
+    {
+        cout << "( " << x << " , " << y << " )" << "在第二象限";
+    }
+    // 第三象限
+    else if (x < 0 && y < 0)
+    {
+        cout << "( " << x << " , " << y << " )" << "在第三象限";
+    }
+
+    // 第四象限
+    else if (x > 0 && y < 0)
+    {
+        cout << "( " << x << " , " << y << " )" << "在第四象限";
+    }
+    else
+    {
+        cout << "输入的不对" << endl;
+    }
+
+    return 0;
+}
